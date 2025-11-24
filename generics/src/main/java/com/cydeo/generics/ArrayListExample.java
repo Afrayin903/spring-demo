a01cc7244806e1eb6c9445c5d3d45b2a66d9dd72
Kahar <afrayin0222@gmail.com>
2025-11-24 16:25:16 -0500

    add new class

---
+++ generics/src/main/java/com/cydeo/generics/ArrayListExample.java
@@ -0,0 +1,20 @@
+package com.cydeo.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+    private static void printDouble (ArrayList<Integer> list){
+        for (int i: list) {
+            System.out.println(i*2);
+        }
+    }
+}
